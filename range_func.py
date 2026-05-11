0ecd65d92152442e260e9d450fd529ebc2e82816
Oloko Olabisi <olokoolabisi.n@gmail.com>
2026-05-11 17:03:03 +0100

    for loop and range  function

---
+++ range_func.py
@@ -0,0 +1,14 @@
+#range with 2 args starts and stops(-1)
+#print numbers from 1 to 10
+for i in range(1, 10):
+	print(i)
+
+#range with 3 args starts, stops(-1), and steps
+print ('2 steps  from 0 to 10')
+for i in range(0, 11, 2):
+	print(i)
+
+#descending for loop with range
+print('count down from 5 to 0')
+for i in range(5, -1, -1):
+	print(i)
