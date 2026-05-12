769eccd5ed66648e13770ebc5e4441c6d9811238
Oloko Olabisi <olokoolabisi.n@gmail.com>
2026-05-12 15:05:08 +0100

    counting one to ten with while loop and for loop

---
+++ oneToTen_forLoop_whileLoop.py
@@ -0,0 +1,12 @@
+# 1 to 10 for loop
+print("1 to 10\n ")
+for i in range(1, 11):
+	print(i)
+print("\n")
+#1 to 10 while loop 
+print("1 to 10\n")
+num = 1
+while num < 11:
+	print(num)
+	num += 1
+
