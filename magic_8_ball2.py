55dc85e068ebafaf5d297496b971f33a083ab057
Oloko Olabisi <olokoolabisi.n@gmail.com>
2026-05-14 15:27:19 +0100

    magic 8 balls, working with lists

---
+++ magic_8_ball2.py
@@ -0,0 +1,5 @@
+import random
+
+messages = ['It  is certain', 'It is  decidedly', 'Reply hazy try again', 'Ask again later', 'concentrate and ask again' , 'My reply is no', 'Outlook not good', 'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
